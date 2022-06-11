92165ec0493e1fe794309a9dad130370ffc37a3e
zhangyu <2863680540@qq.com>
2022-06-11 09:49:55 +0800

    提交消息

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,23 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((options) => {
   options.url = 'http://www.liulongbin.top:3007' + options.url;
+  //注入token
+  if (options.url.includes('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token'),
+    };
+  }
+
+  options.complete = (res) => {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      //  强制清空 token
+      localStorage.removeItem('token');
+      // 强制跳转到登录页面
+      location.href = '/login.html';
+    }
+  };
 });
